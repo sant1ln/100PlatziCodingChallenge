ce8c4414e5fcacbd50d89047154844d3f80fc1be
santiagoln <santiagoln09@gmail.com>
2020-10-24 20:39:15 -0500

    new ex

---
+++ r22/R22.py
@@ -0,0 +1,20 @@
+def calculator_int(saving,interest,time):
+    total = saving * (1+interest/100)**time
+    return round(total,2)
+
+
+
+def compound_interest():
+    saving = 100
+    interest = 4
+
+    interest_time = {
+        '6 months': calculator_int(saving,interest,0.5),
+        '1 year': calculator_int(saving,interest,1),
+        '2 year': calculator_int(saving,interest,2),
+    }
+
+    for i,j in interest_time.items():
+        print(f'{i} => {j}')
+
+compound_interest()
